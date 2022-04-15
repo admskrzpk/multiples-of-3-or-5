cac4c9b0793fc88db241315877dcedeeecd5591f
admskrzpk <100923177+admskrzpk@users.noreply.github.com>
2022-04-15 11:37:53 +0200

    tests update

--- src/main/test/MultiplesEulerSpec.scala
+++ src/main/test/MultiplesEulerSpec.scala
@@ -6,7 +6,7 @@ class MultiplesEulerSpec extends AnyFunSpec {
 
   describe("Range") {
     describe("when boarder is 1000") {
-      it("should have size 233168") {
+      it("should have sum 233168") {
         assert(multiplesEuler(1000) == 233168)
       }
     }
@@ -14,8 +14,17 @@ class MultiplesEulerSpec extends AnyFunSpec {
 
   describe("Range") {
     describe("when boarder is 10") {
-      it("should have size 23") {
+      it("should have sum 23") {
         assert(multiplesEuler(10) == 23)
+      }
+    }
+  }
+
+  describe("Range") {
+    describe("when boarder is < 10 ") {
+      it("should have throw 0") {
+        assert(multiplesEuler(0) == 0)
+        assert(multiplesEuler(-2) == 0)
       }
     }
   }
